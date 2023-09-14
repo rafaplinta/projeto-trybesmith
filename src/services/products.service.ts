7e01d5f37890866d3ef5d860f5e5bb976520d056
Rafaela Plinta <ra.plinta@gmail.com>
2023-09-14 15:48:37 -0300

    Adiciona requisito 3, não o 5. O cinco eu já tinha feito

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -6,30 +6,9 @@ import ProductModel, {
   ProductSequelizeModel,
 } from '../database/models/product.model';
 
-// function validateParams({
-//   name,
-//   price,
-//   orderId,
-// }: ProductInputtableTypes): string | null {
-//   if (!name) return '"name" is required';
-//   if (!price) return '"price" is required';
-//   if (!orderId) return 'OrderId is required';
-//   return null;
-// }
-
 async function create(product: ProductInputtableTypes): Promise<ServiceResponse<Product>> {
-  // let responseService: ServiceResponse<Product>;
-  // const error = validateParams(product);
-
-  // if (error) {
-  //   responseService = { status: 'INVALID_DATA', data: { message: error } };
-  //   return responseService;
-  // }
-
   const newProduct = await ProductModel.create(product);
   return { status: 'SUCCESSFUL', data: newProduct.dataValues };
-
-  // return responseService;
 }
 
 async function findAll(): Promise<ServiceResponse<ProductSequelizeModel[]>> {
